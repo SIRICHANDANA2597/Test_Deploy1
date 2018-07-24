5ed55f5f2afd5bb172f47dbc492df5454041aad7
IBM <IBM@DEVOPS_03>
2018-07-24 15:05:17 +0530

    Changed PATH

---
+++ Cucumber_Selenium/Feature/mytest.feature
@@ -0,0 +1,6 @@
+Feature: to validate login app
+@login
+Scenario: to test login app with valid and invalid data
+Given login app should be available
+When Validatelogin app
+Then login should be successfull
---
+++ Cucumber_Selenium/src/test/java/StepDefinition/StepDefinition.java
@@ -0,0 +1,45 @@
+package StepDefinition;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class StepDefinition {
+	WebDriver driver;
+	String title;
+	
+	@Given("^login app should be available$")
+	public void login_app_should_be_available()  {
+		
+			   driver=new FirefoxDriver();
+			   driver.get("https://opensource-demo.orangehrmlive.com/");
+			   
+	}   
+
+	@When("^Validatelogin app$")
+	public void validatelogin_app() throws InterruptedException  {
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("Admin");
+	    driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("admin123");
+	    driver.findElement(By.xpath("//*[@id='btnLogin']")).click();
+	    title= driver.getTitle();
+	    System.out.println(title);
+
+	    
+	}
+
+	@Then("^login should be successfull$")
+	public void login_should_be_successfull()  {
+		Assert.assertEquals("OrangeHRM", title);
+	    System.out.println("success");
+	}
+
+
+}
